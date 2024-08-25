9e139d40c5c076cf470931edee370c90a697d4c3
chengyi.zhou@acsinternational.edu.sg <chengyi@zhouchengyideMacBook-Air.local>
2024-08-25 17:39:06 +0800

    update leap.c

--- leap.c
+++ leap.c
@@ -1,44 +1,29 @@
 #include <stdio.h>
-#include <string.h>
 int MONTH[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 int L_MONTH[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 char WEEKD[7][3] = {"Su","Mo", "Tu", "We", "Th", "Fr", "Sa"};
-char MONTH_NAME[12][10] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-const int DAYH = 1;
-const int DAYW = 3;
-const int XPAD = 5;
-// 月份间的纵向空白为YPAD
-const int YPAD = 2;
-const int WIDTH = DAYW * 7 * 3 + XPAD * 2;
-const int HEIGHT = (DAYH * 7 + YPAD + 2) * 4;
+char MONTH_NAME[12][10] = {
+    "January", "February", "March", "April", 
+    "May", "June", "July", "August", 
+    "September", "October", "November", "December"
+};
+#define DAYH  1  // 日期高度
+#define DAYW  3  // 日期宽度
+#define XPAD  5  // 月份间的横向空白为XPAD
+#define YPAD  2  // 月份间的纵向空白为YPAD
+#define MWIDTH (XPAD + DAYW * 7)           // 月份宽度
+#define MHEIGHT (YPAD + DAYH * 7 + 2)      //月份高度
+#define WIDTH  (DAYW * 7 * 3 + XPAD * 2)   //总宽度
+#define HEIGHT ((DAYH * 7 + YPAD + 2) * 4) //总高度
 char CALANDAR_BUFFER[HEIGHT][WIDTH];
+
+// 是否闰年
 int leap(int year)
 {
     return (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
-    // int a = year % 100;
-    // if (a == 0)
-    // {
-    //     if (year % 400 == 0)
-    //     {
-    //         return 1;
-    //     }
-    //     else
-    //     {
-    //         return 0;
-    //     }
-    // }
-    // else
-    // {
-    //     if (year % 4 == 0)
-    //     {
-    //         return 1;
-    //     }
-    //     else
-    //     {
-    //         return 0;
-    //     }
-    // }
 }
+
+// 给定日期返回是一年中的第几天
 int calculate(int year, int month, int date)
 {
     int number = 0;
@@ -58,6 +43,7 @@ int calculate(int year, int month, int date)
     return number;
 }
 
+// 给定日期判断是周几
 int weekday(int year, int month, int date)
 {
     int w = 0;
@@ -84,23 +70,20 @@ int weekday(int year, int month, int date)
 
 void print_month_title()
 {
-    int dx = XPAD + DAYW * 7;
-    int dy = YPAD + DAYH * 7 + 2;
     int x0 = 0;
     int y0 = 0;
     for (int y = 0; y < 4; y++)
     {
         for (int x = 0; x < 3; x++)
         {
-            sprintf(&CALANDAR_BUFFER[y0 + dy * y][x0 + dx * x], "%s", MONTH_NAME[x + 3 * y]);
+            sprintf(&CALANDAR_BUFFER[y0 + MHEIGHT * y][x0 + MWIDTH * x], "%s", MONTH_NAME[x + 3 * y]);
             // printf("%d,%d,%d\n", y0 + dy * y, x0 + dx * x, x + 3 * y);
         }
     }
 }
+
 void print_week_title()
 {
-    int dx = XPAD + DAYW * 7;
-    int dy = YPAD + DAYH * 7 + 2;
     int x0 = 0;
     int y0 = 1;
     for (int y = 0; y < 4; y++)
@@ -109,32 +92,34 @@ void print_week_title()
         {
             for (int z = 0; z < 7; z++)
             {
-                sprintf(&CALANDAR_BUFFER[y0 + dy * y][x0 + DAYW * z + (DAYW * 7 + XPAD) * x], "%s", WEEKD[z]);
+                sprintf(&CALANDAR_BUFFER[y0 + MHEIGHT * y][x0 + DAYW * z + MWIDTH * x], "%s", WEEKD[z]);
             }
         }
     }
 }
 
 void print_day(int day, int bx, int by, int x, int y) {
-    int dy = YPAD + DAYH * 7 + 2;
-    int dx = XPAD + DAYW * 7;
-    sprintf(&CALANDAR_BUFFER[y + 2 + dy * by][x * DAYW + bx * dx], "%d", day + 1);
+    sprintf(&CALANDAR_BUFFER[y + 2 + MHEIGHT * by][x * DAYW + bx * MWIDTH], "%d", day + 1);
 }
 
 void print_day_title(int year)
 {
-    int dx = XPAD + DAYW * 7;
-    int dy = YPAD + DAYH * 7 + 2;
     int x = 0;
     int y = 0;
     int count = 0;
+    int l = leap(year);
     for (int by = 0; by < 4; by++)
     {
         for (int bx = 0; bx < 3; bx++)
         {
             x = weekday(year, bx + 3 * by + 1, 1);
             y = 0;
-            int d = MONTH[bx + 3 * by];
+            int d;
+            if (l) {
+                d = L_MONTH[bx + 3 * by];
+            } else {
+                d = MONTH[bx + 3 * by];
+            }
             print_day(0, bx, by, x, y);
             for(int a=1; a<d; a++){
                 if(x==6){
@@ -148,6 +133,7 @@ void print_day_title(int year)
         }
     }
 }
+
 void print_calandar()
 {
     for (int y = 0; y < HEIGHT; y++)
@@ -166,22 +152,9 @@ void print_calandar()
         printf("\n");
     }
 }
+
 int main()
 {
-    // memset(CALANDAR_BUFFER,' ', sizeof(CALANDAR_BUFFER));
-    // for(int y=0;y<HEIGHT;y++){
-    //     for(int x = 0;x<WIDTH; x++){
-    //         CALANDAR_BUFFER[y][x] = 'a'+y%26;
-    //     }
-    // }
-    // int year, month, date;
-    // printf("Type the year,month,date:");
-    // scanf("%d,%d,%d", &year, &month, &date);
-    // int r = weekday(year, month, date);
-    // for (int i = 0; i < 10; i++)
-    // {
-    //     printf("%c", WEEKD[r][i]);
-    // }
     int year = 2024;
     printf("%d\n", year);
     print_month_title();
